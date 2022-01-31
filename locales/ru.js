e1602a4cf85ea7276c3de055f8709226a6b3a12c
HAMMER17 <plutalov70gmail.com>
2022-01-31 15:31:04 +0300

    translet

---
+++ locales/ru.js
@@ -0,0 +1,33 @@
+export default {
+    translation: {
+        h1: 'RSS агрегатор',
+        content: 'Начните читать RSS сегодня! Это легко, это красиво.',
+        text: 'Пример: https://ru.hexlet.io/lessons.rss',
+        placeholder: 'Ссылка RSS',
+        valid: {
+            validError: 'Ссылка должна быть валидным URL',
+        },
+        load: {
+            successful: 'RSS успешно загружен',
+            networkErr: 'Ошибка сети',
+        },
+        parser: {
+            error: 'Ресурс не содержит валидный RSS',
+        },
+        duplication: {
+            error: 'RSS уже существует',
+        },
+        fids: {
+            fid: 'Фиды',
+        },
+        posts: {
+            post: 'Посты',
+            button: 'Просмотр',
+        },
+        bottonForm: 'Добавить',
+        bottonModal: {
+            primary: 'Читать полностью',
+            secondary: 'Закрыть',
+        },
+    },
+};
