7d62c53c69a18087d7fe4896bed790d5dedf40bc
HAMMER17 <plutalov70gmail.com>
2022-02-18 18:55:07 +0300

    add

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import './style.css';
 // import './change.js';
 // import './yup.js';
 // import text from './i18next.js';
+import text from '../locales/ind.js'
 
 const input = document.querySelector('#url-input');
 const button = document.querySelector('#button');
 const out = document.querySelector('.out');
+const select = document.querySelector('#select')
+
+select.addEventListener('click', (e) => {
+  console.log()
+})
 
 button.addEventListener('click', (event) => {
   event.preventDefault();
-  if (input.value === '') {
-    input.classList.add('err');
-    out.innerHTML = 'ошибка ввода данных';
-  }
+  input.value = (window.location.href = '')
+  //   if (input.value === ' ') {
+  //     input.classList.add('err');
+  //     out.innerHTML = 'ошибка ввода данных';
+  //   }
 });
